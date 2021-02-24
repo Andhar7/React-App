89487d7da356b55dc32998fefb15d27d05cc2cc3
Andrej Kling <boxingprofi@web.de>
2021-02-24 22:13:03 +0100

    buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React great apps.</h1>
     <p>Complete course about the best tools and design systems. 
       Prototypes and build something great... </p>
-    <p>Changing again ....one more ...</p>
+    <p>Changing  one more....</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
