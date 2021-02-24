de85fe0e266f14c96416315dee79a532d4055f13
Andrej Kling <boxingprofi@web.de>
2021-02-24 22:06:43 +0100

    changing all

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ const IndexPage = () => (
     <h1>Learn to design and code React great apps.</h1>
     <p>Complete course about the best tools and design systems. 
       Prototypes and build something great... </p>
-    <p>Changing again....</p>
+    <p>Changing again ....one more ...</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
 export default IndexPage
-
