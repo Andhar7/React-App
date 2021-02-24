200b1efebe2ab1a639d3326e3bc94fc8861d0bc9
Andrej Kling <boxingprofi@web.de>
2021-02-24 22:02:01 +0100

    watching button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React great apps.</h1>
     <p>Complete course about the best tools and design systems. 
       Prototypes and build something great... </p>
-    <p>Something great for changing...</p>
+    <p>Changing again....</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
