c1196da828d7e63c5c8cf45e8cda6813f770310a
Andrej Kling <boxingprofi@web.de>
2021-02-24 22:00:08 +0100

    watch button

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Solver´s Design',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and code React great apps.</h1>
+    <p>Complete course about the best tools and design systems. 
+      Prototypes and build something great... </p>
+    <p>Something great for changing...</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
 export default IndexPage
+
